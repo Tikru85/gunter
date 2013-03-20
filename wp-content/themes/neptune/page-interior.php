673c1d80f5fb7ca6df23c7218e747acb11a815e7
mridul89 <mridul_aggarwal@yahoo.co.in>
2013-03-20 18:44:50 +0530

    adding the loop

--- wp-content/themes/neptune/page-interior.php
+++ wp-content/themes/neptune/page-interior.php
@@ -8,49 +8,51 @@
 <?php wp_enqueue_script('jquery'); ?>
 <?php wp_enqueue_script('tinycarousel', get_template_directory_uri() . '/js/jquery.tinycarousel.min.js', array('jquery')); ?>
 <?php get_header(); ?>
-<div id="content" role="main">
-	<?php $images = get_images_ids(true); ?>
-	<?php if(is_array($images) && !empty($images)) : ?>
-		<div class="interior-page">
-			<div id="slideshow-main-image">
-				<img src="<?php echo wp_get_attachment_url($images[0]); ?>"/>
-			</div>
-			<div class="interior-info">
-				<?php the_content(); ?>
-			</div>
-			<div class="clear"></div>
-			<div id="slideshow-thumbnail" class="slider" style="margin-left: 40px;">
-				<div class="viewport">
-					<ul class="overview">
-						<?php foreach($images as $image) : ?>
-							<li><img src="<?php echo wp_get_attachment_url($image); ?>"/></li>
-						<?php endforeach; ?>
-					</ul>
+<?php if(have_posts()) : the_post(); ?>
+	<div id="content" role="main">
+		<?php $images = get_images_ids(true); ?>
+		<?php if(is_array($images) && !empty($images)) : ?>
+			<div class="interior-page">
+				<div id="slideshow-main-image">
+					<img src="<?php echo wp_get_attachment_url($images[0]); ?>"/>
 				</div>
-			</div>
+				<div class="interior-info">
+					<?php the_content(); ?>
+				</div>
+				<div class="clear"></div>
+				<div id="slideshow-thumbnail" class="slider" style="margin-left: 40px;">
+					<div class="viewport">
+						<ul class="overview">
+							<?php foreach($images as $image) : ?>
+								<li><img src="<?php echo wp_get_attachment_url($image); ?>"/></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+				</div>
 
-			<div id="static-thumbnail" class="slider">
-				<div class="viewport">
-					<ul class="overview">
-						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
-						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
-						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
-						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
-					</ul>
+				<div id="static-thumbnail" class="slider">
+					<div class="viewport">
+						<ul class="overview">
+							<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
+							<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
+							<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
+							<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
+						</ul>
+					</div>
 				</div>
+				<div class="clear"></div>
 			</div>
-			<div class="clear"></div>
-		</div>
-		<script>
-			jQuery(document).ready(function($) {
-				$('.slider').each(function() {
-					$(this).tinycarousel({interval:true});
+			<script>
+				jQuery(document).ready(function($) {
+					$('.slider').each(function() {
+						$(this).tinycarousel({interval:true});
+					});
+					$('#slideshow-thumbnail li').click(function() {console.debug('clicked');
+						$('#slideshow-main-image img').attr('src', $(this).find('img').attr('src'));
+					});
 				});
-				$('#slideshow-thumbnail li').click(function() {console.debug('clicked');
-					$('#slideshow-main-image img').attr('src', $(this).find('img').attr('src'));
-				});
-			});
-		</script>
-	<?php endif; ?>
-</div> <!-- content -->
+			</script>
+		<?php endif; ?>
+	</div> <!-- content -->
+<?php endif; ?>
 <?php get_footer(); ?>

8728b44887ba18e8e2ec7e5a4272f07959bb06ed
mridul89 <mridul_aggarwal@yahoo.co.in>
2013-03-20 18:16:38 +0530

    making the images dynamic

--- wp-content/themes/neptune/page-interior.php
+++ wp-content/themes/neptune/page-interior.php
@@ -9,57 +9,48 @@
 <?php wp_enqueue_script('tinycarousel', get_template_directory_uri() . '/js/jquery.tinycarousel.min.js', array('jquery')); ?>
 <?php get_header(); ?>
 <div id="content" role="main">
-	<div class="interior-page">
-		<div id="slideshow-main-image">
-			<img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/>
-		</div>
-		<div class="interior-info">
-			<h3>DESCRIPTION</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed enim orci, molestie eget purus. </p>
-			<h3>DESIGNER</h3>
-			<p>SubZero</p>
-			<h3>PRODUCTS</h3>
-			<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-		</div>
-		<div class="clear"></div>
-		<div id="slideshow-thumbnail" class="slider" style="margin-left: 40px;">
-			<div class="viewport">
-				<ul class="overview">
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-5.jpg" /></li>
-								<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
-					<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-5.jpg" /></li>
-				</ul>
+	<?php $images = get_images_ids(true); ?>
+	<?php if(is_array($images) && !empty($images)) : ?>
+		<div class="interior-page">
+			<div id="slideshow-main-image">
+				<img src="<?php echo wp_get_attachment_url($images[0]); ?>"/>
 			</div>
-		</div>
+			<div class="interior-info">
+				<?php the_content(); ?>
+			</div>
+			<div class="clear"></div>
+			<div id="slideshow-thumbnail" class="slider" style="margin-left: 40px;">
+				<div class="viewport">
+					<ul class="overview">
+						<?php foreach($images as $image) : ?>
+							<li><img src="<?php echo wp_get_attachment_url($image); ?>"/></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
 
-	<div id="static-thumbnail" class="slider">
-		<div class="viewport">
-			<ul class="overview">
-				<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
-				<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
-				<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
-				<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
-			</ul>
+			<div id="static-thumbnail" class="slider">
+				<div class="viewport">
+					<ul class="overview">
+						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-1.jpg"/></li>
+						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-2.jpg" /></li>
+						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-3.jpg" /></li>
+						<li><img src="<?php bloginfo('template_url') ?>/images/Interior-slides/slide-4.jpg" /></li>
+					</ul>
+				</div>
+			</div>
+			<div class="clear"></div>
 		</div>
-	</div>
-		<div class="clear"></div>
-	</div>
-	<script>
-		jQuery(document).ready(function($) {
-			$('.slider').each(function() {
-				$(this).tinycarousel({interval:true});
+		<script>
+			jQuery(document).ready(function($) {
+				$('.slider').each(function() {
+					$(this).tinycarousel({interval:true});
+				});
+				$('#slideshow-thumbnail li').click(function() {console.debug('clicked');
+					$('#slideshow-main-image img').attr('src', $(this).find('img').attr('src'));
+				});
 			});
-			$('#slideshow-thumbnail li').click(function() {console.debug('clicked');
-				$('#slideshow-main-image img').attr('src', $(this).find('img').attr('src'));
-			});
-		});
-	</script>
+		</script>
+	<?php endif; ?>
 </div> <!-- content -->
 <?php get_footer(); ?>
